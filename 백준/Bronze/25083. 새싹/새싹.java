e5e45ad544309a68610dea3c06850a61b1931e04
ljkhyeong <115821049+ljkhyeong@users.noreply.github.com>
2023-12-16 23:45:58 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.io.IOException;
+import java.util.List;
+
+class Main{
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("         ,r'\"7\n"
+			+ "r`-_   ,'  ,/\n"
+			+ " \\. \". L_r'\n"
+			+ "   `~\\/\n"
+			+ "      |\n"
+			+ "      |");
+	}
+
+}
