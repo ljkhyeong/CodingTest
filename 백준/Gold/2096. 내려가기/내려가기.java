33d2945810e730d9557b3948e707c5777ad180d4
ljkhyeong <115821049+ljkhyeong@users.noreply.github.com>
2024-04-02 21:13:39 +0900

    [Gold V] Title: 내려가기, Time: 408 ms, Memory: 52444 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,21 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.io.IOException;
 
-public class Main {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+class Main {
 
-		int n = scan.nextInt();
+
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		int[][] board = new int[n + 1][3];
-		for(int i = 1; i <= n; i++) {
-			for(int j = 0; j < 3; j++) {
-				board[i][j] = scan.nextInt();
-			}
+		int[][] maxDp = new int[n + 1][3];
+		int[][] minDp = new int[n + 1][3];
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+
+		for (int i = 1; i <= n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			board[i][0] = Integer.parseInt(st.nextToken());
+			board[i][1] = Integer.parseInt(st.nextToken());
+			board[i][2] = Integer.parseInt(st.nextToken());
 		}
 
-		int[][] minDp = new int[n + 1][3];
-		int[][] maxDp = new int[n + 1][3];
-		for(int i = 1; i <= n; i++) {
+		for (int i = 1; i <= n; i++) {
 			maxDp[i][0] += Math.max(maxDp[i - 1][0], maxDp[i - 1][1]) + board[i][0];
 			maxDp[i][1] += Math.max(Math.max(maxDp[i - 1][0], maxDp[i - 1][1]), maxDp[i - 1][2]) + board[i][1];
 			maxDp[i][2] += Math.max(maxDp[i - 1][1], maxDp[i - 1][2]) + board[i][2];
@@ -25,12 +34,13 @@ public class Main {
 			minDp[i][2] += Math.min(minDp[i - 1][1], minDp[i - 1][2]) + board[i][2];
 		}
 
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		for(int i = 0; i < 3; i++) {
-			min = Math.min(min, minDp[n][i]);
+		for (int i = 0; i < 3; i++) {
 			max = Math.max(max, maxDp[n][i]);
+			min = Math.min(min, minDp[n][i]);
 		}
+
 		System.out.println(max + " " + min);
 	}
+
+
 }
