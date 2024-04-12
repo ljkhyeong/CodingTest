036f0b405d7492a71cec70424b1c445562e0b4c6
ljkhyeong <115821049+ljkhyeong@users.noreply.github.com>
2024-04-12 21:24:14 +0900

    [Silver II] Title: N과 M (12), Time: 140 ms, Memory: 14600 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,55 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.io.IOException;
+import java.util.List;
+
+class Main {
+
+	static int N, M;
+	static StringBuilder sb = new StringBuilder();
+	static int[] arr, comb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		comb = new int[M];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		dfs(0, 0);
+
+		System.out.println(sb);
+	}
+
+	static void dfs(int start, int depth) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(comb[i]).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+
+		int before = -1;
+		for (int i = start; i < N; i++) {
+			int now = arr[i];
+			if (before != now) {
+				before = now;
+				comb[depth] = now;
+				dfs(i, depth + 1);
+			}
+		}
+	}
+	
+
+}
